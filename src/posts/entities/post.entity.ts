ff3320b0d58597bd674d8e0df3d42b91b03de4e3
Ngoc Ban <daongocban1102@gmail.com>
2022-05-01 17:54:54 +0700

    post schema (#1)

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,16 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Schema as MongoSchema } from 'mongoose';
 
 Schema({ timestamps: true });
 export class Post extends Document {
-  @Prop({ required: true, unique: true })
-  title: string;
+  @Prop({ type: MongoSchema.Types.ObjectId, ref: 'User', required: true })
+  user: MongoSchema.Types.ObjectId;
 
   @Prop({ required: true })
-  content: string;
+  photo_url: string;
 
-  @Prop({ required: true, unique: true })
-  image: string;
+  @Prop({ required: false })
+  description: string;
+
+  @Prop({ required: false })
+  website: string;
+
+  @Prop({ required: true })
+  tags: string;
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post);
