c5ef2e1564ac894e5ee11707d8348b5cc3368074
ngocban284 <daongocban1102@gmail.com>
2022-05-26 23:40:12 +0700

    fix chat (#2)

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,4 +1,3 @@
-import { UserFollowingService } from './user-following/user-following.service';
 import { ChatService } from './chat.service';
 import { Logger, UnauthorizedException } from '@nestjs/common';
 import {
@@ -18,10 +17,7 @@ import { Socket, Server } from 'socket.io';
 export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-  constructor(
-    private readonly chatService: ChatService,
-    private readonly userFollowingService: UserFollowingService,
-  ) {}
+  constructor(private readonly chatService: ChatService) {}
 
   @WebSocketServer()
   server: Server;
