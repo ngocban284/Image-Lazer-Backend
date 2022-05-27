e44c47266c301d038d0d13083f737599e82dd080
Hieu MiLo <ngotrunghieu2k@gmail.com>
2022-05-27 18:32:08 +0700

    [Backend-Realtime] fix authMiddleware (#5)

--- src/chat/authSocket.middleware.ts
+++ src/chat/authSocket.middleware.ts
@@ -1,16 +1,19 @@
+import { UsersService } from './../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Injectable()
 export class AuthSocketMiddleware implements NestMiddleware {
-  constructor(private jwtService: JwtService) {}
-  use(req: Request, res: Response, next: (socketError?: Error) => void) {
+  constructor(
+    private jwtService: JwtService,
+    private usersService: UsersService,
+  ) {}
+  async use(req: Request, res: Response, next: (socketError?: Error) => void) {
     try {
-      const decoded = this.jwtService.decode(
-        Object.values(req.cookies).toString(),
-      );
-      req.user = decoded;
+      const { user_id } = this.jwtService.verify(req.cookies.refreshToken);
+      const user = await this.usersService.getUserById(user_id);
+      req.user = user;
     } catch (error) {
       const socketError = new Error('NOT_AUTHORIZED');
       return next(socketError);
